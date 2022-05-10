887cc01c62f2055493aa2646957c25283c31c0f6
d-w-moore <dmoore@renci.org>
2022-05-10 19:39:44 +0000

    log and exclude over-long values from metadata

--- src/irods_fcs/__init__.py
+++ src/irods_fcs/__init__.py
@@ -1,5 +1,12 @@
 import FlowCal
 
+import logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+FCS_VALUE_MAX_LEN = 2700
+
+
 def load_keywords( filename
         ):
     return FlowCal.io.FCSData(filename).text
@@ -10,7 +17,7 @@ def filter_dict(
         ,keyword_filter = None
         ):
     if keyword_filter is None:
-        return dict_in       
+        dict_out = dict_in
     else:
         dict_out = {}
         for i in keyword_filter:
@@ -20,5 +27,12 @@ def filter_dict(
             else:
                 dict_out.update( (k,v) 
                                  for k,v in dict_in.items() if i(k) )
-        return dict_out
 
+    keys = list(dict_out.keys())
+    for k in keys:
+        v = dict_out[k].encode('utf8')
+        if len(v) > FCS_VALUE_MAX_LEN:
+            logger.warning( "Skipping FCS '%s' keyword since the value exceeds %d bytes", k, FCS_VALUE_MAX_LEN )
+            del dict_out[k]
+
+    return dict_out
